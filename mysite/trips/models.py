7e90913228acaec843c40a85a77f29baad2a431d
Vic <zcon.victor@gmail.com>
2018-09-20 09:56:50 +0800

    fix models

--- mysite/trips/models.py
+++ mysite/trips/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 # class name == table name
 class FX(models.Model):
-    currency_cn = models.CharField(max_length=20, primary_key=True, default=None.)
-    currency_en = models.CharField(max_length=20, default=None.)
-    cash_buying = models.CharField(max_length=20, default=None.)
-    cash_selling = models.CharField(max_length=20, default=None.)
-    spot_buying = models.CharField(max_length=20, default=None.)
-    spot_selling = models.CharField(max_length=20, default=None.)
-    update_date = models.CharField(max_length=20, default=None.)
+    currency_cn = models.CharField(max_length=20, primary_key=True, default=None)
+    currency_en = models.CharField(max_length=20, default=None)
+    cash_buying = models.CharField(max_length=20, default=None)
+    cash_selling = models.CharField(max_length=20, default=None)
+    spot_buying = models.CharField(max_length=20, default=None)
+    spot_selling = models.CharField(max_length=20, default=None)
+    update_date = models.CharField(max_length=20, default=None)
     class Meta:
         managed = True
         db_table = 'FX'#table name
