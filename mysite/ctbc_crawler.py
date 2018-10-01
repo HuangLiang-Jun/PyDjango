9b15c5b0521647f42ac32c96533f2697a71d6df9
Vic <zcon.victor@gmail.com>
2018-10-01 17:26:47 +0800

    chrome test

--- mysite/ctbc_crawler.py
+++ mysite/ctbc_crawler.py
@@ -21,8 +21,8 @@ GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN')
 chrome_options = Options()
 
 chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_BIN
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get(url)
