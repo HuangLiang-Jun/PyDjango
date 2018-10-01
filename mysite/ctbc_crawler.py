8b28926f886025a37da33958cdf91f22f93041ea
Vic <zcon.victor@gmail.com>
2018-10-01 14:40:36 +0800

    chrome setting

--- mysite/ctbc_crawler.py
+++ mysite/ctbc_crawler.py
@@ -16,7 +16,7 @@ url = 'https://www.ctbcbank.com/CTCBPortalWeb/appmanager/ebank/rb?_nfpb=true&_pa
 chrome_options = Options()
 # chrome_options.binary_location = chrome_exec_shim
 chrome_options.add_argument("--headless")       # define headless
-chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 path_of_chrome_driver = '/app/.chromedriver/bin/chromedriver'
