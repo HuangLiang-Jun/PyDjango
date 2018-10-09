01856245eb76c7be7e3d3bac315cb493dd850b80
Vic <zcon.victor@gmail.com>
2018-10-09 12:14:53 +0800

    fix

--- mysite/pttAutoLogin.py
+++ mysite/pttAutoLogin.py
@@ -32,16 +32,16 @@ def pttLogin(account, pwd):
                 content = telnet.read_very_eager().decode('big5','ignore')
 #             print(content)
          
-                if "請按任意鍵繼續" in content:
+                if "請按任意鍵繼續" in content:
                     print("資訊頁面，按任意鍵繼續...")
                     telnet.write(("\r\n").encode('big5') )
                     time.sleep(2)
                     content = telnet.read_very_eager().decode('big5','ignore')
-        elif "請按任意鍵繼續" in content::
-            print("資訊頁面，按任意鍵繼續...")
-            telnet.write(("\r\n").encode('big5') )
-            time.sleep(2)
-            content = telnet.read_very_eager().decode('big5','ignore')
+            elif "請按任意鍵繼續" in content::
+                print("資訊頁面，按任意鍵繼續...")
+                telnet.write(("\r\n").encode('big5') )
+                time.sleep(2)
+                content = telnet.read_very_eager().decode('big5','ignore')
 #              print(content)
 
 kids_json = os.environ.get('Kids', None)
