91e455b68d145514b74e1f9c30a350917be59fde
Vic <zcon.victor@gmail.com>
2018-10-09 12:01:10 +0800

    fix

--- mysite/pttAutoLogin.py
+++ mysite/pttAutoLogin.py
@@ -11,30 +11,26 @@ def pttLogin(account, pwd):
     telnet = telnetlib.Telnet(host)
     time.sleep(1)
     content = telnet.read_very_eager().decode('big5', 'ignore')
-    print('content:', content)
+    # print('content:', content)
 
     if "請輸入代號" in content:
         print('entering account')
         telnet.write((account + "\r\n").encode('big5'))
         time.sleep(1)
         content = telnet.read_very_eager().decode('big5','ignore')
-        print('content:', content)
+        # print('content:', content)
         if "請輸入您的密碼" in content:
             print('entering password')
             telnet.write((pwd + "\r\n").encode('big5'))
             time.sleep(1)
             content = telnet.read_very_eager().decode('big5','ignore')
-            print('content:', content)
+            # print('content:', content)
 
 
 kids_json = os.environ.get('Kids', None)
-print(kids_json)
 kids_dict = json.loads(kids_json)
-print(kids_dict)
 for kid in kids_dict:
     u = kid['user']
     p = kid['pwd']
-    print(u,p)
+    pttLogin(u, p)
 
-    # pttLogin(u, p)
-        
